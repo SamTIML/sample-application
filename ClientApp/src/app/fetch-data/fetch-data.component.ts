c28c21c10975a968d8b0a144a82f17302b1fe8b5
SamTIML <sam.ellis@tattonim.com>
2023-02-27 17:05:34 +0000

    Make forecast dates human readable

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,33 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-@Component({
-  selector: 'app-fetch-data',
-  templateUrl: './fetch-data.component.html'
-})
-export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+import { DateTime } from 'luxon';
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
-  }
+@Component({
+    selector: 'app-fetch-data',
+    templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent implements OnInit {
+    public forecasts: WeatherForecast[];
+
+    constructor(
+        private http: HttpClient,
+        @Inject('BASE_URL') private baseUrl: string
+    ) { }
+
+    ngOnInit() {
+        this.http.get<WeatherForecast[]>(`${this.baseUrl}weatherforecast`).subscribe(forecasts => {
+            for (const forecast of forecasts) {
+                forecast.date = DateTime.fromISO(forecast.date).toLocaleString(DateTime.DATE_MED);
+            }
+            this.forecasts = forecasts;
+        }, error => console.error(error));
+    }
 }
 
 interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
