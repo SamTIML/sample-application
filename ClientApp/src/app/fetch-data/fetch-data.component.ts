37614f9110f07a6b90bf9949a581154ed23e7ef7
SamTIML <sam.ellis@tattonim.com>
2023-02-28 09:41:28 +0000

    Replace summary text with images

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -9,6 +9,18 @@ import { DateTime } from 'luxon';
 })
 export class FetchDataComponent implements OnInit {
     public forecasts: WeatherForecast[];
+    private imgLookup: [string, string][] = [
+        ['Freezing', 'snowflake.png'],
+        ['Bracing', 'snowflake.png'],
+        ['Chilly', 'snowflake.png'],
+        ['Cool', 'clouds.png'],
+        ['Mild', 'clouds.png'],
+        ['Warm', 'sun.png'],
+        ['Balmy', 'sun.png'],
+        ['Hot', 'sun.png'],
+        ['Sweltering', 'fire.png'],
+        ['Scorching', 'fire.png']
+    ]
 
     constructor(
         private http: HttpClient,
@@ -19,6 +31,12 @@ export class FetchDataComponent implements OnInit {
         this.http.get<WeatherForecast[]>(`${this.baseUrl}weatherforecast`).subscribe(forecasts => {
             for (const forecast of forecasts) {
                 forecast.date = DateTime.fromISO(forecast.date).toLocaleString(DateTime.DATE_MED);
+                for (const img of this.imgLookup) {
+                    if (img[0] === forecast.summary) {
+                        forecast.summary = img[1];
+                        break;
+                    }
+                }
             }
             this.forecasts = forecasts;
         }, error => console.error(error));
